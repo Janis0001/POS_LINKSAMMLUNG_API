a25a0b88b1b41b97a534c56e192a02fbc6407b65
Janis0001 <janis.papai@htl-saalfelden.at>
2023-06-06 01:40:58 +0200

    Revert "Full Project"
    This reverts commit 12f0cd0eb43165a68d4b1e08b017de8444484c48.

--- Link_Sammlung/src/main/java/com/example/link_sammlung/LinkSammlungRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.link_sammlung;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinkSammlungRepository extends MongoRepository<Link, String> {
-        //public Link findByLink(String linktype);
-}
-
