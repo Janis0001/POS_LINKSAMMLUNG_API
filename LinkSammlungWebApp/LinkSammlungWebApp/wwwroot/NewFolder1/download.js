d227a687df99b2d1a3205fe62e93cd4d5db75819
Janis0001 <janis.papai@htl-saalfelden.at>
2023-06-06 12:57:59 +0200

    NEW

--- LinkSammlungWebApp/LinkSammlungWebApp/wwwroot/NewFolder1/download.js
+++
@@ -1,9 +0,0 @@
-﻿window.downloadFile = function (fileName, content) {
-    const element = document.createElement('a');
-    const file = new Blob([content], { type: 'text/plain' });
-    element.href = URL.createObjectURL(file);
-    element.download = fileName;
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
